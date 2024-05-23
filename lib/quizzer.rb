e4ad81d7b9235ce4129dc26e3c39fe54718782c8
Joseph Smith <joseph.smit95@gmail.com>
2024-05-23 16:53:23 -0400

    added rspec testing

--- lib/quizzer.rb
+++ lib/quizzer.rb
@@ -1,34 +1,45 @@
 require 'tty-prompt'
+require 'yaml'
 
 class Quizzer
-    def initialize
-        @prompt = TTY::Prompt.new
+  def initialize
+    @prompt = TTY::Prompt.new
+    @quizzes_dir = File.expand_path("../../quizzes", __FILE__)  # this needs to be absolute path
+  end
+
+  def start
+    quiz_names = list_quizzes
+    if quiz_names.empty?
+      puts "No quizzes found in the quizzes directory."
+      return
+    end
+    quiz_name = @prompt.select("Which quiz would you like to take?", quiz_names)
+    take_quiz(quiz_name)
+  end
+
+  private
+
+  def list_quizzes
+    quizzes = Dir["#{@quizzes_dir}/*.yaml"].map { |file| File.basename(file, ".yaml") }
+    puts "Available quizzes: #{quizzes.inspect}"  # Debugging line
+    quizzes
+  end
+
+  def take_quiz(quiz_name)
+    questions = load_quiz(quiz_name)#.shuffle #shuffle the questions array
+    score = 0
+
+    questions.each do |question|
+        shuffled_choices = question["choices"]#.shuffle    
+        answer = @prompt.select(question["question"], shuffled_choices)
+        score += 1 if answer == question["correct"]
     end
 
-    def start
-        quiz_name = @prompt.select("Which quiz would you like to take?", list_quizzes)
-        take_quiz(quiz_name)
-    end
+    puts "You scored #{score}/#{questions.size}!"
+  end
 
-    private
-    def list_quizzes
-        Dir["quizzes/*.yaml"].map{|file| File.basename(file,".yaml")}
-    end
-
-    def take_quiz(quiz_name)
-        questions = load_quiz(quiz_name)
-        score = 0
-
-        questions.each do |question|
-            answer = @prompt.select(question[:question], question[:choices])
-            score += 1 if answer == question[:correct]
-        end
-
-        puts "you scored #{score}/#{questions.size}!"
-    end
-    
-    def load_quiz(quiz_name)
-        file_path = "quizzes/#{quiz_name}.yaml"
-        YAML.load_file(file_path)
-    end
+  def load_quiz(quiz_name)
+    file_path = "#{@quizzes_dir}/#{quiz_name}.yaml"
+    YAML.load_file(file_path)
+  end
 end
---
+++ spec/quizzer_spec.rb
@@ -0,0 +1,48 @@
+require 'rspec'
+require 'tty-prompt'
+require_relative '../lib/quizzer'
+
+RSpec.describe Quizzer do
+  let(:quizzer) { Quizzer.new }
+  let(:prompt) { instance_double(TTY::Prompt) }
+  let(:sample_questions) do
+    [
+      {
+        "question" => "What is the capital of France?",
+        "choices" => ["Paris", "London", "Berlin", "Madrid"],
+        "correct" => "Paris"
+      },
+      {
+        "question" => "Who wrote 'To Kill a Mockingbird'?",
+        "choices" => ["Harper Lee", "Mark Twain", "Ernest Hemingway", "F. Scott Fitzgerald"],
+        "correct" => "Harper Lee"
+      }
+    ]
+  end
+
+  before do
+    allow(TTY::Prompt).to receive(:new).and_return(prompt)
+    allow(prompt).to receive(:select).with("What is the capital of France?", ["Paris", "London", "Berlin", "Madrid"]).and_return("Paris")
+    allow(prompt).to receive(:select).with("Who wrote 'To Kill a Mockingbird'?", ["Harper Lee", "Mark Twain", "Ernest Hemingway", "F. Scott Fitzgerald"]).and_return("Harper Lee")
+    allow(YAML).to receive(:load_file).and_return(sample_questions)
+  end
+
+  describe '#list_quizzes' do
+    it 'returns a list of quiz names' do
+      allow(Dir).to receive(:[]).and_return(["/path/to/quizzes/sample_quiz.yaml"])
+      expect(quizzer.send(:list_quizzes)).to eq(['sample_quiz'])
+    end
+  end
+
+  describe '#load_quiz' do
+    it 'loads the questions for a given quiz name' do
+      expect(quizzer.send(:load_quiz, 'sample_quiz')).to eq(sample_questions)
+    end
+  end
+
+  describe '#take_quiz' do
+    it 'runs through the questions and calculates the score' do
+      expect { quizzer.send(:take_quiz, 'sample_quiz') }.to output(/You scored 2\/2!/).to_stdout
+    end
+  end
+end
